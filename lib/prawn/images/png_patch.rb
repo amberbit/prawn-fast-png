e1b3de72756eb99f68afe2fc7434bbafc1c0c761
Wojciech Piekutowski <wojciech@piekutowski.net>
2009-08-19 01:18:34 +0200

    Support 16-bit PNGs

--- lib/prawn/images/png_patch.rb
+++ lib/prawn/images/png_patch.rb
@@ -22,8 +22,18 @@ module Prawn
         # get only one color value per pixel (Intensity) for grayscale+alpha images
         format = color_type == 4 ? 'I' : 'RGB'
 
-        img_data      = img.export_pixels_to_str(0, 0, width, height, format)
-        alpha_channel = img.export_pixels_to_str(0, 0, width, height, 'A')
+        case bits
+        when 8
+          img_data      = img.export_pixels_to_str(0, 0, width, height, format)
+          alpha_channel = img.export_pixels_to_str(0, 0, width, height, 'A')
+        when 16
+          # export_pixels_to_str returns little-endian data, but we need big-endian
+          # so it's usually more efficient to use export_pixels and pack
+          img_data      = img.export_pixels(0, 0, width, height, format).pack('n*')
+          alpha_channel = img.export_pixels(0, 0, width, height, 'A').pack('n*')
+        else
+          raise Errors::UnsupportedImageType, "Can't handle #{img.depth}-bit PNG images"
+        end
 
         @img_data      = Zlib::Deflate.deflate(img_data)
         @alpha_channel = Zlib::Deflate.deflate(alpha_channel)
