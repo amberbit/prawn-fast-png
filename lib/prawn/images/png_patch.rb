9be1f4ba0e2ae548f72cc5e6b951904887749cd3
Wojciech Piekutowski <wojciech@piekutowski.net>
2009-08-19 13:07:20 +0200

    Tidy up

--- lib/prawn/images/png_patch.rb
+++ lib/prawn/images/png_patch.rb
@@ -18,6 +18,8 @@ module Prawn
 
       def unfilter_image_data
         img = Magick::Image.from_blob(@prawn_fast_png_data).first
+        # image blob not needed anymore, let GC take care of it
+        @prawn_fast_png_data = nil
 
         # get only one color value per pixel (Intensity) for grayscale+alpha images
         format = color_type == 4 ? 'I' : 'RGB'
@@ -32,14 +34,11 @@ module Prawn
           img_data      = img.export_pixels(0, 0, width, height, format).pack('n*')
           alpha_channel = img.export_pixels(0, 0, width, height, 'A').pack('n*')
         else
-          raise Errors::UnsupportedImageType, "Can't handle #{img.depth}-bit PNG images"
+          raise Errors::UnsupportedImageType, "Can't handle #{bits}-bit PNG images"
         end
 
         @img_data      = Zlib::Deflate.deflate(img_data)
         @alpha_channel = Zlib::Deflate.deflate(alpha_channel)
-
-        # image blob not needed anymore, let GC take care of it
-        @prawn_fast_png_data = nil
       end
     end
   end
